6b7d0c64e2dc9bd3109e415c130fa4cc7d687ce9
cvargsh <carlos.vargas.sistemas@gmail.com>
2021-11-30 17:41:28 -0500

    primer Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+
+const port = process.env.PORT | 4123
+app.use((req, res, next) =>{
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+})
+app.use(cors())
+
+app.listen(port, ()=>{
+    console.log("Server running on" + port)
+})
